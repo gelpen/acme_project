6f0e338bfd3a31e7efd3b4c1d8a478892cc653e2
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2024-12-07 13:21:31 +0500

    ok 07122024 1321

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -39,6 +39,10 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+# Импортируем класс пагинатора.
+from django.core.paginator import Paginator
+
+from django.views.generic import ListView
 
 
 # Добавим опциональный параметр pk.
@@ -54,7 +58,8 @@ def birthday(request, pk=None):
         instance = None
     # Передаём в форму либо данные из запроса, либо None.
     # В случае редактирования прикрепляем объект модели.
-    form = BirthdayForm(request.POST or None, instance=instance)
+    form = BirthdayForm(request.POST or None,
+                        files=request.FILES or None, instance=instance)
     # Остальной код без изменений.
     context = {'form': form}
     # Сохраняем данные, полученные из формы, и отправляем ответ:
@@ -67,12 +72,39 @@ def birthday(request, pk=None):
     return render(request, 'birthday/birthday.html', context)
 
 
-def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+# def birthday_list(request):
+#     # Получаем все объекты модели Birthday из БД.
+#     birthdays = Birthday.objects.all()
+#     # Передаём их в контекст шаблона.
+#     context = {'birthdays': birthdays}
+#     return render(request, 'birthday/birthday_list.html', context)
+
+# def birthday_list(request):
+#     # Получаем список всех объектов с сортировкой по id.
+#     birthdays = Birthday.objects.order_by('id')
+#     # Создаём объект пагинатора с количеством 10 записей на страницу.
+#     paginator = Paginator(birthdays, 2)
+
+#     # Получаем из запроса значение параметра page.
+#     page_number = request.GET.get('page')
+#     # Получаем запрошенную страницу пагинатора.
+#     # Если параметра page нет в запросе или его значение не приводится к числу,
+#     # вернётся первая страница.
+#     page_obj = paginator.get_page(page_number)
+#     # Вместо полного списка объектов передаём в контекст
+#     # объект страницы пагинатора
+#     context = {'page_obj': page_obj}
+#     return render(request, 'birthday/birthday_list.html', context)
+
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 2
+
 
 def delete_birthday(request, pk):
     # Получаем объект модели или выбрасываем 404 ошибку.
