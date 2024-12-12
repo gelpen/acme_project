5c9effb4257e7a6da62555e57785ff3ea2c0de71
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2024-12-12 23:50:57 +0500

    ok 12122024 2350

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -4,7 +4,13 @@ from django.conf import settings
 # Импортируем функцию, позволяющую серверу разработки отдавать файлы.
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+
+
+# Добавьте новые строчки с импортами классов.
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+from django.urls import include, path, reverse_lazy
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -13,6 +19,16 @@ urlpatterns = [
 
     # Подключаем urls.py приложения для работы с пользователями.
     path('auth/', include('django.contrib.auth.urls')),
+
+    path(
+        'auth/registration/', 
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 
     # В конце добавляем к списку вызов функции static.
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
